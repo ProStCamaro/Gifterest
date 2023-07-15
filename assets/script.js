97469f0b70ea0691855e9aa1f7ca5c51e936844e
Joe FRE <josephfrechette02@gmail.com>
2023-07-14 20:55:40 -0700

    added an event listener

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var topic = document.getElementById("input");
+var btn = document.getElementById("searchBtn");
+var PexelsApiKey = 'OwkM6MPGAgrOf7q4aEXCiSMCh6nDXqlwRGPrFY2wqDthBf0djDILv9mg'
+var requestPexels = `https://api.pexels.com/v1/search?page=${topic}&rating=g&api_key=${PexelsApiKey}`;
+var GiphyAPIkey = "tZuspRklb8ygicJMNGrPxVwcWBYhVH3R";
+var PexelsParent = document.getElementById("pexels-parent");
+var GiphyParent = document.getElementById("giphy-parent");
+
+btn.addEventListener("click", function() {
+    sendGiphyApiRequest();
+})
+
+function sendGiphyApiRequest() {
+    var topic = document.getElementById("input");
+    var requestGiphy = `https://api.giphy.com/v1/gifs/search?q=${topic}&rating=g&api_key=${GiphyAPIkey}`;
+    fetch(requestGiphy).then(function(data){
+        return data.json()
+    })
+    .then(function(json){
+        console.log(json.data[0].images.fixed_height.url)
+        var gifPath = json.data[0].images.fixed_height.url;
+        var gif = document.createElement("gif");
+        gif.setAttribute("src", gifPath)
+        document.GiphyParent.appendChild(gif);
+    })
+};
+
+function sendPexelsApiRequest() {
+    console.log(topic)
+    fetch(requestPexels).then(function(data){
+        return data.json()
+    })
+    .then(function(json){
+        console.log(json.data[0].images.fixed_height.url)
+        var imgPath = json.data[0].images.fixed_height.url;
+        var img = document.createElement("img");
+        img.setAttribute("src", imgPath)
+        document.PexelsParent.appendChild(img);
+    });
+}
+
